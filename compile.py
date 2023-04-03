9cb2c111d770dbfb9be8a820b3d834d0ad820d06
Connor King <sigstackfault@gmail.com>
2023-04-03 14:10:06 -0400

    compiler

---
+++ compile.py
@@ -0,0 +1,67 @@
+import glob
+import yaml
+import pprint
+
+BASE_DIRECTORIES = [
+    "components",
+    "missiles",
+    "stations",
+    "resources",
+]
+
+filenames = []
+for directory in BASE_DIRECTORIES:
+    filenames.extend(
+        glob.glob(f"{directory}/*.fw")
+    )
+    filenames.extend(
+        glob.glob(f"{directory}/**/*.fw")
+    )
+
+result = {
+    "resources": {},
+    "components": {},
+    "combat_stations": {},
+    "missiles": {},
+}
+
+for filename in filenames:
+    print(f"{filename}...")
+    with open(filename) as f:
+        current = yaml.load(f, Loader=yaml.SafeLoader)
+        if("resources" in current):
+            result["resources"].update(
+                current["resources"]
+            )
+        if("component" in current):
+            result["components"].update(
+                current["component"]
+            )
+        if("components" in current):
+            result["components"].update(
+                current["components"]
+            )
+        if("missile" in current):
+            result["missiles"].update(
+                current["missile"]
+            )
+        if("missiles" in current):
+            result["missiles"].update(
+                current["missiles"]
+            )
+        if("missiles" in current):
+            result["missiles"].update(
+                current["missiles"]
+            )
+        if("combat_station" in current):
+            result["combat_stations"].update(
+                current["combat_station"]
+            )
+        if("combat_stations" in current):
+            result["combat_stations"].update(
+                current["combat_stations"]
+            )
+
+pprint.pprint(result)
+with open("junctspace.yaml", "w") as f:
+    yaml.dump(result, f, Dumper=yaml.SafeDumper, sort_keys=False)
